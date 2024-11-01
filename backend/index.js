2dbb176185cab9561b5ec25ac815ce57a00c30b9
Syed Ali. Imran <aliimran@aptech.com>
2024-11-01 12:09:35 +0500

    roles API Done

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app  = express();
+
+require("dotenv").config();
+
+
+// middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+// -- Connection
+
+
+const {connectionDB} = require('./Config/connectionDB')
+
+
+
+//--Models
+
+const {
+    getRoles,
+    createRoles,
+    deleteRole,
+    updateRole
+} = require("./Controllers/UserRolesController")
+
+
+
+// ROLES [ GET , POST ]
+
+app.route("/userroles").get(getRoles).post(createRoles);
+
+// ROLES [ DELETE , UPDATE ]
+
+app.route("/userroles/:id").delete(deleteRole).put(updateRole);
+
+
+
+
+
+app.listen(process.env.PORT,function(){
+
+    console.log(`SERVER IS RUNNING ON THE PORT ${process.env.PORT}`);
+    connectionDB();
+
+})
